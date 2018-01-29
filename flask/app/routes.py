517fddf44a0448234f2c9a40ba033ce36b6d0d65
Daniel Njoo <danielnjoo@gmail.com>
2018-01-28 21:19:43 -0500

    flask step3

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from flask import request
+from flask import jsonify
 from app import app
+
+posts = []
 
 @app.route('/', methods=['GET', 'POST'])
 def microblog():
@@ -9,3 +12,17 @@ def microblog():
         user_input = request.get_data().split('=')[1]
         return render_template('index.html', title='Home', name=user_input)
     return render_template('index.html', title='Home')
+
+@app.route('/blog', methods=['GET', 'POST'])
+def blog():
+    if request.method == "POST":
+        name = request.form['name']
+        comment = request.form['comment']
+        print "name:", name
+        print "comment:", comment
+
+        posts.append({'name': name, 'comment': comment})
+        return render_template('blog.html', title='Blog', posts=posts)
+
+
+    return render_template('blog.html', title='Blog')
