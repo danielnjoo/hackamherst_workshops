58f391ebe26c94a09c1ec1df1c6a67253b8dce2a
Daniel Njoo <danielnjoo@gmail.com>
2018-01-28 20:44:50 -0500

    flask step2

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template
+from flask import request
 from app import app
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def microblog():
+    if request.method == "POST":
+        print request.get_data()
+        user_input = request.get_data().split('=')[1]
+        return render_template('index.html', title='Home', name=user_input)
     return render_template('index.html', title='Home')
