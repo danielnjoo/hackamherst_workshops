ea3d168e60195fbf9b482d4d7768ecac6f0fc84b
Nathaniel Corley <nscorley@gmail.com>
2018-02-01 12:31:42 -0500

    Step 2

--- reactnative/LoginPage/App.js
+++ reactnative/LoginPage/App.js
@@ -1,45 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput } from 'react-native';
+import { Button } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          The Nellie, a cruising yawl, swung to her anchor without a flutter of
-          the sails, and was at rest. The flood had made, the wind was nearly
-          calm, and being bound down the river, the only thing for it was to
-          come to and wait for the turn of the tide.
-        </Text>
-        <Text>
-          The sea-reach of the Thames stretched before us like the beginning of
-          an interminable waterway. In the offing the sea and the sky were
-          welded together without a joint, and in the luminous space the tanned
-          sails of the barges drifting up with the tide seemed to stand still in
-          red clusters of canvas sharply peaked, with gleams of varnished
-          sprits. A haze rested on the low shores that ran out to sea in
-          vanishing flatness. The air was dark above Gravesend, and farther back
-          still seemed condensed into a mournful gloom, brooding motionless over
-          the biggest, and the greatest, town on earth.
-        </Text>
-        <Text>
-          The Director of Companies was our captain and our host. We four
-          affectionately watched his back as he stood in the bows looking to
-          seaward. On the whole river there was nothing that looked half so
-          nautical. He resembled a pilot, which to a seaman is trustworthiness
-          personified. It was difficult to realize his work was not out there in
-          the luminous estuary, but behind him, within the brooding gloom.
-        </Text>
-      </View>
+      <ImageBackground
+        style={styles.backgroundImage}
+        source={{ uri: 'https://www.natezeman.com/images/xl/0551_NZ_Sky_Dance_WM.jpg' }}
+      >
+        <View style={styles.formContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.titleText}>TRAVEL THE WORLD</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              placeholderTextColor="white"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="white"
+              secureTextEntry={true}
+            />
+          </View>
+          <Button
+            title='LOG IN'
+            buttonStyle={styles.button}
+            textStyle={{ fontWeight: 'bold', color: 'white' }}
+          />
+        </View>
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  formContainer: {
+    height: 200,
+    justifyContent: 'space-around',
+  },
+  inputContainer: {
+    height: 100,
+    justifyContent: 'space-around',
+
+  },
+  input: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 0.5,
+    color: 'white',
+    padding: 7,
+  },
+  button: {
+    height: 50,
+    width: 250,
+    backgroundColor: 'transparent',
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 30,
+  },
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '900',
+  },
+
 });
